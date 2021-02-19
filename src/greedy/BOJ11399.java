9ca48bdbd6607c5a1ebcf3a7dd075be08fcfb8b4
gom20 <rhaldud89@gmail.com>
2021-02-19 12:58:47 +0900

    Add BOJ11399

---
+++ src/greedy/BOJ11399.java
@@ -0,0 +1,36 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11399 {
+
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int max = 0;
+		int multiply = n;
+		for(int i = 0; i < n; i++) {
+			max = max + arr[i]*multiply;
+			multiply--;
+		}
+		
+		System.out.println(max);
+		
+	}
+	
+}
