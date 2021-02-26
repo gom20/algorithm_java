ded4546578cd86d5923a21243605f64cd8f8ba89
gom20 <rhaldud89@gmail.com>
2021-02-27 00:01:15 +0900

    Add Template

--- src/Template.java
+++ src/Template.java
@@ -5,5 +5,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Template {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
