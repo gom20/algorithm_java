b7eaf894f5c52755de3a3a651e064a9112e83714
Miyoung <rhaldud89@gmail.com>
2021-11-29 13:20:05 +0900

    Commit BOJ2110

--- binarysearch/BOJ2110.java
+++ binarysearch/BOJ2110.java
@@ -22,10 +22,12 @@ public class BOJ2110 {
         int left = 1; // 최소 인접 거리
         int right = houses[N-1] - houses[0]; // 최대 인접 거리
 
+        int result = 0;
         while(left <= right){
             int mid = (left + right)/2;
             if(getPossibleCnt(houses, mid) >= C){
                 // 공유기 더 많이 설치된다. 인접 거리 더 늘리자.
+                result = Math.max(result, mid);
                 left = mid+1;
             } else {
                 // 공유기 설치 불가능. 인접 거리를 좁혀야 함.
@@ -33,7 +35,7 @@ public class BOJ2110 {
             }
         }
 
-        System.out.println(left-1);
+        System.out.println(result);
 
     }
 
